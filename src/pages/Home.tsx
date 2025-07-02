07f3f59a64fc30f117335ee58edfeb4732ad1c49
Debora Leticia Ferreira Maciel <debora.ferreira@mplantech.com.br>
2025-07-02 16:18:09 +0100

    upp

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -70,7 +70,7 @@ function Home() {
       <Dialog open={!!pandaMessage} onClose={() => setPandaMessage('')}>
         <div className='flex flex-col items-center justify-center p-5'>
           <h2 className='text-2xl font-bold mb-4'>Panda Arrependido</h2>
-          <p className='text-base mb-4 text-left bg-gray-200 shadow-inner rounded-lg p-2'>{pandaMessage}</p>
+          <p className='text-base mb-4 text-left bg-gray-200 shadow-inner rounded-lg p-2'>{pandaMessage.replace(/"/g, '').replace(/'/g, '')}</p>
           <p className='text-md text-gray-500 mb-2'>Panda's mood: <span className='font-semibold'>{pandaMood}</span></p>
           <button
             className='button text-black dark:text-white'
