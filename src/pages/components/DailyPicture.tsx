17e3915832445ec9621d6870e8a2b22a13ad9e44
Debora Leticia Ferreira Maciel <debora.ferreira@mplantech.com.br>
2025-07-03 19:37:08 +0100

    upping

--- src/pages/components/DailyPicture.tsx
+++ src/pages/components/DailyPicture.tsx
@@ -12,8 +12,12 @@ export default function DailyPicture({ setOpenDailyPicture, dailyPictures, openD
     return (
         <Dialog fullWidth={true} open={openDailyPicture} onClose={() => setOpenDailyPicture(false)}>
             <div className='flex flex-col items-center justify-center p-5 rounded-full'>
-                <h2 className='text-2xl font-light mb-4'>Panda's Picture of the Day</h2>
-                <DailyPictureSwipe dailyPictures={dailyPictures}/>
+                <h2 className='text-2xl font-bold mb-2'>Panda's Picture of the Day</h2>
+                <h2 className="text-base font-light text-center text-gray-600 italic mb-2">
+                    (Don’t judge the panda’s photography skill)
+                </h2>
+
+                <DailyPictureSwipe dailyPictures={dailyPictures} />
                 {/* <div className="rounded-xl border border-black/10 shadow-md">
                     <img className="w-[300px] rounded-t-lg" src={`https://raw.githubusercontent.com/debora-maciel/trin-forgive-me/refs/heads/main/images/${dailyPicture}`} />
                     <p className="text-sm p-2">{dailyPictureComment}</p>
