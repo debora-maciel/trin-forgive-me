85df87328b55b1352bd2bc3c8914857b3960249a
Debora Leticia Ferreira Maciel <debora.ferreira@mplantech.com.br>
2025-07-06 11:14:10 +0100

    up

--- src/pages/FuckOff.tsx
+++ src/pages/FuckOff.tsx
@@ -1,9 +1,14 @@
 export default function Fuckoff() {
     return (
         <div>
-            I'm sorry for not giving you security <br/>
-            I forgive you for the lack of communication 
+            I'm sorry for not giving you security <br />
+            I forgive you for the lack of communication <br />
             and clarity about what you were feeling.
+
+            {/* my god ure just an avoidant :')
+            that's so funny we could've worked out.
+            so sad that u cant handle intimacy
+            we were so hot together :/ */}
         </div>
     )
 }
